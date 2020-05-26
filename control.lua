0e86fa002396ad848940ec21dd13be58f03f4e39
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-05-26 10:48:01 +0200

    Cleanup code

--- control.lua
+++ control.lua
@@ -3,30 +3,11 @@ script.on_event(defines.events.on_lua_shortcut,
             if(event.prototype_name == "trolley-problem") then
                 local player = game.get_player(event.player_index)
                 local called = 0
+
                 for i, train in pairs(player.surface.get_trains(player.force)) do
 
-                    -- check if at least 1 locomotive on the entire train has the same color as the player
-                    local soulbound = false
-                    for i, movers in pairs(train.locomotives) do
-                        for i, locomotive in pairs(movers) do
-                            if(player.color.r == locomotive.color.r and player.color.g == locomotive.color.g and player.color.b == locomotive.color.b) then
-                                soulbound = true
-                            end
-                        end
-                    end
-
-                    if soulbound then
-
-                        -- check if the player is standing on a rail tile
-                        local rail = nil
-                        if not rail then
-                            rail = player.surface.find_entity("straight-rail", player.position)
-                        end
-                        if not rail then
-                            rail = player.surface.find_entity("curved-rail", player.position)
-                        end
-
-                        if rail then
+                    if train_matches_player(train, player) then
+                        if has_track_below_player(player)  then
 
                             local schedule = {}
                             schedule.current = 1
@@ -37,13 +18,14 @@ script.on_event(defines.events.on_lua_shortcut,
                             end
 
                             local record = {}
-                            record.rail = rail
+                            record.rail = get_track_below_player(player)
                             record.wait_conditions = {
                                 {compare_type="or", type = "inactivity", ticks = 60 * 15},
                                 {compare_type="or", type = "inactivity", ticks = 60 *  5},
                                 {compare_type="and", type = "passenger_present"},
                             }
                             record.temporary = true;
+
                             table.insert(schedule.records, 1, record)
                             train.schedule = schedule;
                             train.go_to_station(1)
@@ -58,3 +40,25 @@ script.on_event(defines.events.on_lua_shortcut,
         end
 )
 
+function train_matches_player(train, player)
+    for i, movers in pairs(train.locomotives) do
+        for i, locomotive in pairs(movers) do
+            if(identical_color(player.color, locomotive.color)) then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+function identical_color(color1, color2)
+    return color1.r == color2.r and color1.g == color2.g and color1.b == color2.b
+end
+
+function get_track_below_player(player)
+    return player.surface.find_entity("straight-rail", player.position) or player.surface.find_entity("curved-rail", player.position) or nil
+end
+
+function has_track_below_player(player)
+    return get_track_below_player(player) and true
+end
